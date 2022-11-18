68514542d8b3f6cdc2804bef2b8127729e20fa63
laurabotel <laura.e.botel@gmail.com>
2022-11-18 12:41:00 -0500

    adds Juice class to be used for adding instances of Juice Recipes and their benefits

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,18 +2,38 @@ import Health from './Health';
 import gut from './images/gut.jpg';
 import heart from './images/heart.jpg';
 import skin from './images/skin.jpg';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        <Health imageSource={gut} healthArea='Gut Health'></Health>
-        <Health imageSource={heart} healthArea='Heart Health'></Health>
-        <Health imageSource={skin} healthArea='Skin Health'></Health>
-        <Health imageSource={gut} healthArea='Colon Health'></Health>
-        <Health imageSource={skin} healthArea='Brain Health'></Health>
+        <Health
+          imageSource={gut}
+          healthArea='Gut Health'
+          juiceDetails={{}}
+        ></Health>
+        <Health
+          imageSource={heart}
+          healthArea='Heart Health'
+          juiceDetails={{}}
+        ></Health>
+        <Health
+          imageSource={skin}
+          healthArea='Skin Health'
+          juiceDetails={{}}
+        ></Health>
+        <Health
+          imageSource={gut}
+          healthArea='Colon Health'
+          juiceDetails={{}}
+        ></Health>
+        <Health
+          imageSource={skin}
+          healthArea='Brain Health'
+          juiceDetails={{}}
+        ></Health>
       </header>
     </div>
   );
---
+++ my-app/src/data/recipes.js
@@ -0,0 +1,11 @@
+/* 
+This file holds our recipes and exports the recipes as an array
+- each recipe is represented by an object with several properties:
+  - juiceName - 'string'
+  - healthAreas - Array of 'strings'
+  - juiceRecipe - Array of objects that include ingredientName qty properties
+  - healthBenefits - 'string'
+*/
+const recipes = [];
+
+export default recipes;
