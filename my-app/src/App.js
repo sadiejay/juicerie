44e860ba975c71a8e92dd3c406258490183d7a7e
laurabotel <laura.e.botel@gmail.com>
2022-11-18 12:59:49 -0500

    adds 2 recipes to recipes.js, adds useState hook to App.js to be incorporated in generating Health components dynamically

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,20 @@
+import useState from 'react';
+
+// import components
 import Health from './Health';
+
+// import images & data
+import recipes from './data/recipes.js';
 import gut from './images/gut.jpg';
 import heart from './images/heart.jpg';
 import skin from './images/skin.jpg';
-// import logo from './logo.svg';
+
+// import styles
 import './App.css';
 
 function App() {
+  const [juices, setJuices] = useState(recipes);
+
   return (
     <div className='App'>
       <header className='App-header'>
