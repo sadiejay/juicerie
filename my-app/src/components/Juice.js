f76888ff59c4ca576b120f4c2c1c0401c06606e2
Sadie <19538219+sadiejay@users.noreply.github.com>
2022-11-18 20:50:28 -0800

    Adds juice card list and juice card

--- my-app/src/components/Juice.js
+++ my-app/src/components/Juice.js
@@ -1,5 +1,15 @@
 function Juice(props) {
   // should display a clickable card with an image, and the title of the juice
+
+  console.log(props);
+  return (
+    <div>
+      <img
+      alt="juice"
+      />
+      <p>{props.details.juiceName}</p>
+    </div>
+  );
 }
 
 export default Juice;
